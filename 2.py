cd2bb0771c100ecb0255865de57f366ce0390765
Sergey_Puchnin <puchnin1996@mail.ru>
2023-05-19 11:44:36 +0300

    В задачу 2 добавили решение

--- 2.py
+++ 2.py
@@ -4,3 +4,12 @@
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0)
 
+number = int(input('Введите число: '))
+num = number
+sum = 0
+
+while (num > 0):
+    sum = sum + num % 10
+    num = num // 10
+
+print(f'Сумма цифр в числе {number} -> {sum}')
